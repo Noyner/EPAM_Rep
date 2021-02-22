f844cc932f2eb5c1d3ac50854114f541c57da618
Noyner <basonmark4@gmail.com>
2021-02-23 00:07:39 +0400

    Added 1.1.4

--- Task 1/Task 1_1/Task 1_1/Program.cs
+++ Task 1/Task 1_1/Task 1_1/Program.cs
@@ -7,16 +7,16 @@ namespace Task_1_1
         static void Main(string[] args)
         {
 
-            Rectangle();              // 1.1.1
-            Triangle();               // 1.1.2
-            AnotherTriangle();          // 1.1.3
-                                        // 1.1.4
-                                        // 1.1.5
-                                        // 1.1.6
-                                        // 1.1.7
-                                        // 1.1.8
-                                        // 1.1.9
-                                        // 1.1.10
+            //Rectangle();              // 1.1.1
+            //Triangle();               // 1.1.2
+            //AnotherTriangle();       // 1.1.3
+            XmasTree();                            // 1.1.4
+                                                   // 1.1.5
+                                                   // 1.1.6
+                                                   // 1.1.7
+                                                   // 1.1.8
+                                                   // 1.1.9
+                                                   // 1.1.10
 
         }
 
@@ -59,11 +59,43 @@ namespace Task_1_1
 
         static void AnotherTriangle()
         {
+            Console.Write("Введите высоту ёлки: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < height; i++)
+            {
+                for (int space = 0; space < height - i - 1; space++)
+                {
+                    Console.Write(" ");
+                }
 
+                for (int tree = 0; tree < i * 2 + 1; tree++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+
+
+            }
+            Console.ReadKey();
         }
 
+        static void XmasTree()
+        {
+            Console.Write("Введите высоту многоярусной ёлки: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+            for (int i=0; i<=height;i++)
+            {
+                for (int j=0; j<i; j++)
+                {
+                    string str = new String('*', j);
+                    Console.WriteLine(str.PadLeft(height+3)+ "*" + str);
+
+                }
+            }
+
+        }
+      
+
     }
-}
-
-
 }
