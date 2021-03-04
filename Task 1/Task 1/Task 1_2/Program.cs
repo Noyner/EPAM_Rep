4531b4495e71f1cae7b0fb5d821cc71b732c928c
HOME-PC\Sgt.Pepper <basonmark4@gmail.com>
2021-03-04 18:05:25 +0400

    Added 1.2.1

--- Task 1/Task 1/Task 1_2/Program.cs
+++ Task 1/Task 1/Task 1_2/Program.cs
@@ -6,7 +6,22 @@ namespace Task_1_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Результат не округляю. Для округления поменять тип данных sum и average на int.
+
+            float sum = 0;
+            float average = 0;
+            Console.WriteLine("Введите предложение: " + Environment.NewLine);
+            string s = Console.ReadLine();
+            string[] str = s.Split(new[] { ' ', ':', ',', '!', '?', '.', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string k in str)
+            {
+                sum+=k.Length;
+                average = sum / str.Length;
+            }
+
+            Console.WriteLine("Средняя длина слова в данной строке: {0} букв ", average);
+            Console.ReadKey();
+
         }
     }
 }
