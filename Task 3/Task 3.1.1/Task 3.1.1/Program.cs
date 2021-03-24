0dd018e905c98f1a8ff436cbd5d487c4f47e9683
HOME-PC\Sgt.Pepper <basonmark4@gmail.com>
2021-03-24 22:47:11 +0400

    Started 3.1.1

---
+++ Task 3/Task 3.1.1/Task 3.1.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_3._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество участников: ");
+            int N = Int32.Parse(Console.ReadLine());
+
+            List<int> peoples = new List<int>(N) { };
+
+            for (int i = 1; i < N+1; i++)
+            {
+                peoples.Add(i);
+            }
+
+            //foreach (int k in peoples)
+            //{
+            //    Console.WriteLine(k);
+            //}
+
+            for (int i = 1; i <= peoples.Count; i++)
+            {
+                peoples.Remove(peoples[1]);
+                Console.WriteLine("Удалён {0}", peoples[1]);
+                Console.ReadKey();
+                foreach(int k in peoples)
+                {
+                    Console.WriteLine(k);
+                    Console.ReadKey();
+                }
+            }
+
+
+
+        }
+    }
+}
