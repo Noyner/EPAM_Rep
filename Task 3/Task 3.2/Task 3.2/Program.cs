fb268ae69377ab0e344173e7ec6e9381bdfff403
HOME-PC\Sgt.Pepper <basonmark4@gmail.com>
2021-04-02 18:53:18 +0400

    Started 3.2

---
+++ Task 3/Task 3.2/Task 3.2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicArray<int> newD = new DynamicArray<int>();
+            //newD.Add(1);
+        }
+    }
+
+    class DynamicArray<T>
+    {
+        public T[] array;
+        public int ArrayCapacity;
+        public int count = 1;
+        public DynamicArray()
+        {
+            ArrayCapacity = 8;
+            array = new T[8];
+        }
+
+        public DynamicArray(int ArrayCapacity)
+        {
+            Console.WriteLine("Введите ёмкость массива: ");
+            ArrayCapacity = Int32.Parse(Console.ReadLine());
+            array = new T[ArrayCapacity];
+        }
+
+        //public DynamicArray(IEnumerable<T>) ----------- 3
+        //{
+
+        //}
+
+        public void Add (T element)
+        {
+            array[count] = element;
+            count++;
+        }
+
+        public IEnumerable<T> AddRange()
+        {
+            return null;
+        }
+
+        public void Remove()
+        {
+
+        }
+
+        public int Lenght => array.Length;
+        public int Capacity
+        {
+            get
+            {
+                
+                return ArrayCapacity;     
+            }
+
+            private set
+            {
+                Capacity = ArrayCapacity;
+            }
+        }
+    }
+}
